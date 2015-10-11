f3396e96b2184a0d9547bbdb6d9cf01226b2aa9e
Kamron Batman <kamronbatman@users.noreply.github.com>
2015-10-10 18:35:46 -0700

    [Fix] Fixes an error in the recursive function name

--- nQueensSolutionsTweet.js
+++ nQueensSolutionsTweet.js
@@ -1,1 +1,1 @@
-function nQueensSolution(n,o,u,e,r){for(var s=(1<<n)-1,t="",i=~(u|e|r);i&s;R=q(n,o?o+","+a:a,(u|a)>>1,e|a,(r|a)<<1),t+=t&&R?"|"+R:R)i-=a=i&-i;return e==s?o:t}
+function nQueensSolution(n,o,u,e,r){for(var s=(1<<n)-1,t="",i=~(u|e|r);i&s;R=nQueensSolution(n,o?o+","+a:a,(u|a)>>1,e|a,(r|a)<<1),t+=t&&R?"|"+R:R)i-=a=i&-i;return e==s?o:t}
