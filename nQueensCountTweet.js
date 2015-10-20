9f0d02f45bff41ac72d07f83a4c3897f77723323
Kamron Batman <kamron.batman@gmail.com>
2015-10-19 17:26:00 -0700

    [Refactor] Refactored minified code so that nQueensCount uses 106 characters, and nQueensSolutions uses 138 characters.

--- nQueensCountTweet.js
+++ nQueensCountTweet.js
@@ -1,3 +1,3 @@
 // Thank you Makersquare LA for everything you have done for me.
-// This 112 character solution in a tweet is for you!
-function M(K,S,L,A){for(var a=0,t=(1<<K)-1,u=~(S|L|A);u&t;a+=M(K,(S|b)>>1,L|b,(A|b)<<1))u-=b=u&-u;return a|L==t}
+// This 106 character solution in a tweet is for you!
+M=(K,S,L,A)=>{for(var a=0,t=(1<<K)-1,u=~(S|L|A);u&t;a+=M(K,(S|b)>>1,L|b,(A|b)<<1))u-=b=u&-u;return a|L==t}
