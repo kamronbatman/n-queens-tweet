3ddc120c7dc4b6f6ef2e74810b09a2d657e3c2c3
Kamron Batman <kamronbatman@users.noreply.github.com>
2015-11-24 16:46:36 -0800

    Update nQueensSolutionsTweet.js

--- nQueensSolutionsTweet.js
+++ nQueensSolutionsTweet.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 q=(n,o,u,e,r)=>{for(var s=(1<<n)-1,t="",i=~(u|e|r);i&s;R=q(n,o?o+","+a:a,(u|a)>>1,e|a,(r|a)<<1),t+=t&&R?"|"+R:R)i-=a=i&-i;return e==s?o:t}
-=======
-function nQueensSolution(n,o,u,e,r){for(var s=(1<<n)-1,t="",i=~(u|e|r);i&s;R=nQueensSolution(n,o?o+","+a:a,(u|a)>>1,e|a,(r|a)<<1),t+=t&&R?"|"+R:R)i-=a=i&-i;return e==s?o:t}
->>>>>>> f3396e96b2184a0d9547bbdb6d9cf01226b2aa9e
