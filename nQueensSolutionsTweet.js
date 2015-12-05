f908720ef29ce92463aa369e5ac5e7ff1bc479ea
Kamron Batman <kamronbatman@users.noreply.github.com>
2015-12-05 15:50:01 -0800

    Updates with MKSLA parameters.

--- nQueensSolutionsTweet.js
+++ nQueensSolutionsTweet.js
@@ -1,1 +1,1 @@
-q=(n,o,u,e,r)=>{for(var s=(1<<n)-1,t="",i=~(u|e|r);i&s;R=q(n,o?o+","+a:a,(u|a)>>1,e|a,(r|a)<<1),t+=t&&R?"|"+R:R)i-=a=i&-i;return e==s?o:t}
+q=(M,K,S,L,A)=>{for(var s=(1<<M)-1,t="",i=~(S|L|A);i&s;R=q(M,K?K+","+a:a,(S|a)>>1,L|a,(A|a)<<1),t+=t&&R?"|"+R:R)i-=a=i&-i;return L==s?K:t}
